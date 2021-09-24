169e5efcc5acfeeffd1afd11904a8d3aa6218a29
bajenob <jonjill1488@gmail.com>
2021-09-24 19:09:57 +0300

    Add project files.

---
+++ SystemCOnsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SystemCOnsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
